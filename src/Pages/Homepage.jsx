82baf3cf63ddd56c20ce4c0cdaa27ba7dd34a302
khalid <khalidcomilla58@gmail.com>
2024-11-11 05:29:21 +0600

    books aligned

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -10,7 +10,7 @@ import truncate from '../Utils/Truncate';
 import { AuthContext } from '../Providers/AuthProvider';
 import axios from 'axios';
 import Spinner from '../Components/Spinner';
-import Books from '../Components/Books';
+import Books from '../Components/Books';   
 
 
 function Homepage() {
@@ -36,7 +36,7 @@ function Homepage() {
           loading || !ready ? <Spinner></Spinner>:
           <div>
             <Banner books = {books}></Banner>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8 gap-4 m-4'>
+            <div className='grid justify-items-center grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8  my-4'>
             {
               books.map(book => 
                   <Books user = {user} book = {book} key = {book._id}></Books>
