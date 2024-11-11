25344754871637f9aa32bae3c547b1f74ffbbf2a
khalid <khalidcomilla58@gmail.com>
2024-11-11 07:38:10 +0600

    button change

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
         <div className="text-center text-white px-6 py-4 md:px-16 md:py-8">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">Unleash Your thrist for knowledge</h1>
           <p className="text-lg md:text-xl mb-6 font-bold">Explore exclusive books tailored <span className=' text-red-500'>just</span> for you.</p>
-          <Link to="/available" className="bg-yellow-500 text-black py-3 px-8 rounded-lg text-lg font-semibold hover:bg-yellow-600 transition duration-300">Shop Now</Link>
+          <Link to="/available" className="bg-red-500 text-white py-3 px-8 rounded-lg text-lg font-semibold hover:bg-red-600 transition duration-300">Shop Now</Link>
         </div>
       </div>
     </div>
